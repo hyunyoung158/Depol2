81533f276715f200383b9cb5dbed8410e6fda1f3
hyunyoung158 <setarong158@gmail.com>
2014-01-21 13:12:10 +0900

    스크롤뷰 수정 완료

--- PortfolioViewController.m
+++ PortfolioViewController.m
@@ -5,23 +5,44 @@
 //  Created by Lee HyunYoung on 2014. 1. 20..
 //  Copyright (c) 2014년 SDT-1. All rights reserved.
 //
+// 포트폴리오 메인 화면
+// 스크롤뷰 등록해서 3개의 뷰컨트롤러를 넣는다.
+// 버튼으로 스크롤 뷰 이동한다.
 
 #import "PortfolioViewController.h"
 #import "PortfolioCenterTabViewController.h"
 #import "PortfolioLeftTabViewController.h"
 #import "PortfolioRightTabViewController.h"
 
-@interface PortfolioViewController ()
+@interface PortfolioViewController ()<UIScrollViewDelegate>
 
-@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (strong, nonatomic) UIScrollView *scrollView;
 @end
 
 @implementation PortfolioViewController
 
 
+- (IBAction)selectTab1:(id)sender {
+    [self.scrollView scrollRectToVisible:CGRectMake(self.scrollView.frame.origin.x, self.scrollView.frame.origin.y,
+                                                    self.scrollView.frame.size.width, self.scrollView.frame.size.height)
+                                         animated:YES];
+}
+- (IBAction)selectTab2:(id)sender {
+    [self.scrollView scrollRectToVisible:CGRectMake(self.scrollView.frame.origin.x + 320, self.scrollView.frame.origin.y,
+                                                    self.scrollView.frame.size.width, self.scrollView.frame.size.height)
+                                         animated:YES];
+
+}
+- (IBAction)selectTab3:(id)sender {
+    [self.scrollView scrollRectToVisible:CGRectMake(self.scrollView.frame.origin.x + 640, self.scrollView.frame.origin.y,
+                                                    self.scrollView.frame.size.width, self.scrollView.frame.size.height)
+                                         animated:YES];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 20, 320, 416)];
     PortfolioLeftTabViewController *leftVC = [[UIStoryboard storyboardWithName:@"Main" bundle:nil]instantiateViewControllerWithIdentifier:@"tab1"];
     PortfolioCenterTabViewController *centerVC = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"tab2"];
     PortfolioRightTabViewController *rightVC = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"tab3"];
@@ -46,6 +67,9 @@
     self.scrollView.showsVerticalScrollIndicator = NO;
     self.scrollView.alwaysBounceHorizontal = NO;
     self.scrollView.alwaysBounceVertical = NO;
+    self.scrollView.bounces = NO;
+    
+    [self.view addSubview:self.scrollView];
 }
 
 - (void)didReceiveMemoryWarning
